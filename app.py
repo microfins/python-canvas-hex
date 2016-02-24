d04d76816f52f648013eae72a4e2c032649828d5
boknows <lboland@fool.com>
2016-02-24 11:17:40 -0500

    Added Flask-JSON to handle requests. Implemented the shell of defining a map in /createMap

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, jsonify
-import redis
+from flask import Flask, render_template, request, json, jsonify
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+import redis, random, urllib2
 
 app = Flask(__name__)
+json = FlaskJSON(app)
 r = redis.Redis("localhost")
 
 @app.route("/")
@@ -11,9 +13,51 @@ def main():
 @app.route('/save',methods=['POST'])
 def save():
     if request.method == "POST":
-        r.set("test", request.json)
-        result = eval(r.get("test"))
+        r.set("test", pickle.dumps(request.json))
+        result = pickle.loads(r.get("test"))
         return jsonify(result)
+
+@app.route('/createMap',methods=['POST'])
+def createMap():
+    data = request.get_json(force=True)
+    cols = int(data['cols'])
+    rows = int(data['rows'])
+    height = int(data['height'])
+    mapList = []
+    mapProps = {}
+    terrains = ['grassland', 'mountains', 'water', 'tundra']
+    colors = {
+        "tundra": "white",
+        "grassland": "green",
+        "water": "blue",
+        "mountains": "brown"
+    }
+    for col in range(0, cols):
+        mapList.append([])
+        for row in range(0, rows):
+            currentTerrain = random.choice(terrains)
+            mapList[col].append({
+                "h": False,
+                "row": row,
+                "col": col,
+                "x": None,
+                "y": None,
+                "t": currentTerrain,
+                "tc": colors[currentTerrain],
+                "txt": str(col) + "," + str(row),
+                "bor": {
+                    "n": None,
+                    "s": None,
+                    "nw": None,
+                    "sw": None,
+                    "ne": None,
+                    "se": None
+                },
+                "own": None,
+                "u": None,
+            })
+
+    return json_response(map=mapList, props=mapProps)
 
 if __name__ == "__main__":
     app.run(debug=True)
